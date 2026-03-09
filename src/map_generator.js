8c10e743b22fbefb865c882f3c3541d1d3aca57b
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2026-03-09 16:48:01 -0400

    Clear out noise functions

--- src/map_generator.js
+++ src/map_generator.js
@@ -58,46 +58,10 @@ class ValueNoise extends NoiseGenerator {
 class PerlinNoise extends NoiseGenerator {
   constructor() {
     super();
-    this.grid = [];
-    const cols = NUM_CELLS + 1;
-    const rows = NUM_CELLS + 1;
-
-    for (let y = 0; y < rows; y++) {
-      let row = [];
-      for (let x = 0; x < cols; x++) {
-        const angle = Math.random() * Math.PI * 2;
-        row.push({ x: Math.cos(angle), y: Math.sin(angle) });
-      }
-      this.grid.push(row);
-    }
   }
 
   getNoise(tileX, tileY) {
-    const nx = (tileX * NUM_CELLS) / MAP_SIZE;
-    const ny = (tileY * NUM_CELLS) / MAP_SIZE;
-    const cx = Math.floor(nx);
-    const cy = Math.floor(ny);
-    const u = nx - cx;
-    const v = ny - cy;
-
-    const g00 = this.grid[cy][cx];
-    const g10 = this.grid[cy][cx + 1];
-    const g01 = this.grid[cy + 1][cx];
-    const g11 = this.grid[cy + 1][cx + 1];
-
-    const d00 = dotProduct(g00.x, g00.y, u, v);
-    const d10 = dotProduct(g10.x, g10.y, u - 1, v);
-    const d01 = dotProduct(g01.x, g01.y, u, v - 1);
-    const d11 = dotProduct(g11.x, g11.y, u - 1, v - 1);
-
-    const uSmooth = fade(u);
-    const vSmooth = fade(v);
-
-    const top = lerp(d00, d10, uSmooth);
-    const bottom = lerp(d01, d11, uSmooth);
-    const val = lerp(top, bottom, vSmooth);
-
-    return (val + 1) / 2; // Normalize to 0..1
+    throw new Error("TODO");
   }
 }
 
